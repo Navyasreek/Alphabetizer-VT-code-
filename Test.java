a246211368767d31d8457dd5119a25cff4c9e499
Navyasreek <navyasree.ksr16@gmail.com>
2018-09-26 23:58:15 -0500

    Input through Text file

--- Test.java
+++ Test.java
@@ -1,32 +1,43 @@
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
+		FileReader file = new FileReader("C:\\Users\\Navya\\Desktop\\Testdata.txt");
+		BufferedReader br = new BufferedReader(file);
 
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the string");
-		String str = sc.nextLine();
-		List<String> cList = new ArrayList<String>();
+		String str;
+		try {
+			while ((str = br.readLine()) != null) {
 
-		str = str.replaceAll("[^a-zA-Z]", "");
-		int len = str.length();
-		for (int i = 0; i < len; i++) {
-			cList.add(str.substring(0, 1));
-			str = str.substring(1);
+				List<String> cList = new ArrayList<String>();
+
+				str = str.replaceAll("[^a-zA-Z]", "");
+				int len = str.length();
+				for (int i = 0; i < len; i++) {
+					cList.add(str.substring(0, 1));
+					str = str.substring(1);
+				}
+				String result = "";
+				Collections.sort(cList, String.CASE_INSENSITIVE_ORDER);
+				for (String s : cList) {
+					result += s;
+
+				}
+				System.out.println(result);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		String result = "";
-		Collections.sort(cList, String.CASE_INSENSITIVE_ORDER);
-		for (String s : cList) {
-			result += s;
-
-		}
-		System.out.println(result);
-		sc.close();
+		// sc.close();
 
 	}
 
